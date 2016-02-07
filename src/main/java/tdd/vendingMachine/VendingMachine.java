59d4097e1138cf2e92c74c4dafb9081d6978fac7
Pawel Kaczorowski <Pawel.Kaczorowski@IGT.com>
2016-02-07 12:23:25 +0100

    Exception handling refactoring

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -10,11 +10,14 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.function.Consumer;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Lists.newArrayList;
 import static tdd.vendingMachine.util.NumberUtils.isEqualOrGreaterThan;
 
 public class VendingMachine {
 
+    private final CoinContainer coinInputTray = new CoinContainer();
     private final CoinContainer stash = new CoinContainer();
     private final CoinContainer cassette = new CoinContainer();
     private final CoinContainer coinOutputTry = new CoinContainer();
@@ -30,76 +33,100 @@ public class VendingMachine {
     }
 
     public void insertCoin(Coin coin) {
-        try {
+        checkNotNull(coin);
+
+        performActionAndHandleException(() -> {
+            coinInputTray.add(coin);
             coinValidator.validate(coin);
-            stash.add(coin);
+            coinInputTray.transferAllCoinsTo(stash);
             tryToBuyProduct();
-        } catch (UnsupportedCoinException e) {
-            coinOutputTry.add(coin);
-        }
+        });
     }
 
     public String getMessageFromDisplay() {
         return display.getMessage();
     }
 
-    public void addProduct(int i, Product product) {
-        productStorage.addProduct(i, product);
+    public void addProduct(int shelfNumber, Product product) {
+        checkArgument(shelfNumber > 0);
+        checkNotNull(product);
+
+        productStorage.addProduct(shelfNumber, product);
     }
 
     public void selectShelf(int shelfNumber) {
-        Shelf shelf = productStorage.getShelf(shelfNumber);
-        if (shelf.isNotEmpty()) {
-            selectedShelf = Optional.of(shelf);
-            tryToBuyProduct();
-        }
+        checkArgument(shelfNumber > 0);
+
+        performActionAndHandleException(() -> {
+            Shelf shelf = productStorage.getShelf(shelfNumber);
+            if (shelf.isNotEmpty()) {
+                selectedShelf = Optional.of(shelf);
+                tryToBuyProduct();
+            }
+        });
     }
 
     public Collection<Product> getOutputProductsTray() {
         return outputProductsTry;
     }
 
-    private void tryToBuyProduct() {
-        try {
-            if (selectedShelf.isPresent()) {
-                Shelf shelf = selectedShelf.get();
-                if (isEqualOrGreaterThan(stash.getTotalAmount(), shelf.getProductPrice())) {
-                    BigDecimal changeAmount = stash.getTotalAmount().subtract(shelf.getProductPrice());
-                    if (!changeAmount.equals(BigDecimal.ZERO)) {
-                        cassette.transferCoinsByAmountTo(changeAmount, coinOutputTry);
-                        stash.transferAllCoinsTo(cassette);
-                        shelf.dropProductTo(outputProductsTry);
-                    }
-                    setToDefaultStateWithMessage(Display::clear);
-                }else{
-                    display.setMessage(getRequiredToCoverProductPriceAmount());
-                }
-            } else {
-                display.setMessage(stash.getTotalAmount());
-            }
-        } catch (CantGiveTheChangeException e) {
-            display.setMessage(e.getMessage());
-            setToDefaultStateWithMessage(d -> d.setMessage(e.getMessage()));
-        }
-    }
-
-    private BigDecimal getRequiredToCoverProductPriceAmount(){
-        return selectedShelf
-                .map(shelf -> shelf.getProductPrice().subtract(stash.getTotalAmount()))
-                .orElseThrow(() -> new RuntimeException("No shelf is selected"));
-    }
-
     public void cancel() {
-        setToDefaultStateWithMessage(Display::clear);
+        resetToDefaultStateWithMessage(Display::clear);
     }
 
     public void addCoinsToCassette(Collection<Coin> coins) {
+        checkNotNull(coins);
+
         coins.stream().forEach(cassette::add);
     }
 
-    private void setToDefaultStateWithMessage(Consumer<Display> displayConsumer){
+    private void performActionAndHandleException(Action action) {
+        try {
+            action.perform();
+        } catch (UnsupportedCoinException e) {
+            coinInputTray.transferAllCoinsTo(coinOutputTry);
+        } catch (CantGiveTheChangeException e) {
+            resetToDefaultStateWithMessage(d -> d.setMessage(e.getMessage()));
+        }
+    }
+
+    private void tryToBuyProduct() {
+        if (selectedShelf.isPresent()) {
+            Shelf shelf = selectedShelf.get();
+            if (isEqualOrGreaterThan(stash.getTotalAmount(), shelf.getProductPrice())) {
+                buyProductFromShelf(shelf);
+                resetToDefaultStateWithMessage(Display::clear);
+            } else {
+                display.setMessage(getRequiredToCoverProductPriceAmount());
+            }
+        } else {
+            display.setMessage(stash.getTotalAmount());
+        }
+    }
+
+    private void buyProductFromShelf(Shelf shelf) {
+        BigDecimal changeAmount = stash.getTotalAmount().subtract(shelf.getProductPrice());
+        if (!changeAmount.equals(BigDecimal.ZERO)) {
+            cassette.transferCoinsByAmountTo(changeAmount, coinOutputTry);
+            stash.transferAllCoinsTo(cassette);
+            shelf.dropProductTo(outputProductsTry);
+        }
+    }
+
+    private BigDecimal getRequiredToCoverProductPriceAmount() {
+        return selectedShelf
+            .map(shelf -> shelf.getProductPrice().subtract(stash.getTotalAmount()))
+            .orElseThrow(() -> new RuntimeException("No shelf is selected"));
+    }
+
+    private void resetToDefaultStateWithMessage(Consumer<Display> displayConsumer) {
         stash.transferAllCoinsTo(coinOutputTry);
         selectedShelf = Optional.empty();
         displayConsumer.accept(display);
+    }
+
+    @FunctionalInterface
+    interface Action {
+        void perform();
     }
 }
